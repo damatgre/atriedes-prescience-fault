c26e8f94fcc9e425b2226e64ff33b2b2595fe906
damatgre <gregdamato2@gmail.com>
2021-05-14 22:04:26 -0400

    attempt to add id through jquery

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,26 @@
-
-//GIVEN I am using a daily planner to create a schedule
-
 //current date displayed
 var now = JSON.stringify(moment().format("dddd MMMM, YYYY"));
 $("#currentDay").append(now);
 
 //WHEN I scroll down
 //THEN I am presented with time blocks for standard business hours
-
+var time = JSON.stringify(moment().format("HH"));
 
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
+const timeSlot =["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
+const isLargeNumber = (element) => element = time;
+
+//testing to find index that matches time
+time123 = timeSlot.find(isLargeNumber);
+
+console.log(time123);
+
+//for loop for time check
+for (i = 0; i < timeSlot.length; i++) {
+    
+}
 
 //WHEN I click into a time block
 //THEN I can enter an event
