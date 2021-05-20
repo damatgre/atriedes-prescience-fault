a700b2c41c349ace260c4e3f0824afb4a1252691
damatgre <gregdamato2@gmail.com>
2021-05-20 17:11:21 -0400

    get local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,28 +9,47 @@ var time = parseInt(moment().format("HH"));
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
-$(".time-block").each(function(){
+//this takes place at the load of the website. 
+//WHEN I click into a time block
+//THEN I can enter an event
+$(".time-block").each(function () {
     var timeId = parseInt($(this)
         .attr("id")
         .split("-")[1]
     );
 
-    if (timeId < time){
+    if (timeId < time) {
         $(this).find('input').addClass('past');
     } else if (timeId === time) {
         $(this).find('input').addClass('present');
     } else {
         $(this).find('input').addClass('future');
     }
+
+    //finding the id for the div that matches localStorage
+    var keyId = $(this).attr("id");
+    //retrieving item associated with id
+    var text = localStorage.getItem(keyId);
+    //defining place to put content
+    var content = $(this).find("input").val();
+
+    if (content !== null) {
+        $(this).find("input").val(text);
+    }
+
 });
-
-//WHEN I click into a time block
-//THEN I can enter an event
-
-
 
 //WHEN I click the save button for that time block
 //THEN the text for that event is saved in local storage
+$(".saveBtn").on("click", function() {
+    var timeBox = $(this).parent().attr("id");
+    var inputBox = $(this).siblings(".textarea").val();
+    localStorage.setItem(timeBox, inputBox);
+
+});
+
+
+
 
 
 //WHEN I refresh the page
